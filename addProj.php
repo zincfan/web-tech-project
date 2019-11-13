4c01213e1bafe8772a3fc06c36a456dba30b87b3
utkarshfz <44747398+utkarshfz@users.noreply.github.com>
2019-11-13 15:42:37 +0530

    Add files via upload

---
+++ addProj.php
@@ -0,0 +1,24 @@
+<?php
+include "config.php";
+$username=$_SESSION['username'];
+
+$target_folder=$_POST['proj_name'];
+mkdir("proj_record/$target_folder");
+
+$sql="CREATE TABLE $target_folder (contributers VARCHAR(100) NOT NULL,PRIMARY KEY (contributers))";
+mysqli_query($con, $sql);
+
+$sql="INSERT INTO $target_folder (contributers) VALUES ('$username')";
+mysqli_query($con, $sql);
+
+$sql="INSERT INTO proj_list (project_name,contributers) VALUES ('$target_folder','$username')";
+
+$option=$_POST['privacy'];
+
+if($option=="public")
+{
+mysqli_query($con, $sql);
+}
+
+echo "Created Successfully";
+?>
