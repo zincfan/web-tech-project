a02b1fb4937756e5b098ce0705bb8d22ad1c571a
utkarshfz <44747398+utkarshfz@users.noreply.github.com>
2019-11-16 22:38:04 +0530

    Add files via upload

---
+++ login_network.php
@@ -0,0 +1,27 @@
+<?php 
+include "config.php";
+
+    $uname = $_POST['username'];
+    $password =$_POST['password'];
+
+    if ($uname != "" && $password != "")
+    {
+
+        $sql_query = "select count(*) as cntUser from DB where username='".$uname."' and password='".$password."'";
+        $result = mysqli_query($con,$sql_query);
+        $row = mysqli_fetch_array($result);
+
+        $count = $row['cntUser'];
+
+        if($count > 0)
+        {
+            $_SESSION['username'] = $uname;
+            $_SESSION['password']= $password;
+             header('Location:networks.php');
+        }
+        else
+        {
+            echo "Invalid username and password";
+        }
+        }
+?>
